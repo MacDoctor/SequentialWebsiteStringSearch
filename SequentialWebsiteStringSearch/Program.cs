8b6d01378b72f3c482076c7b6cce3f16a40a073a
Mike2012 <Mike2012>
2016-11-07 22:41:34 -0500

    renaming complete

---
+++ SequentialWebsiteStringSearch/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://github.com/gtg154i
+
+using System;
+using System.Windows.Forms;
+
+namespace SequentialWebsiteStringSearch
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
